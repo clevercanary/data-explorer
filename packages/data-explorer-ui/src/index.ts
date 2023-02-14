3519724c20c5f903a9aaa9bc8a6324840f305bb8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-02-14 17:28:45 +1000

    feat: update main index imports (#679)

--- packages/data-explorer-ui/src/index.ts
+++ packages/data-explorer-ui/src/index.ts
@@ -1,9 +1,1 @@
 export type {} from "../types/data-explorer-ui";
-export * from "./components/common/Dot/dot";
-export * from "./hooks/useBreakpointHelper";
-export * from "./theme/common/breakpoints";
-export * from "./theme/common/components";
-export * from "./theme/common/palette";
-export * from "./theme/common/shadows";
-export * from "./theme/common/typography";
-export * from "./viewModelBuilders/common/utils";
