ca7204bc4a6e0c7ea7d00fa3ea453b489935797c
Fran McDade <frano-m@users.noreply.github.com>
2023-03-02 16:38:30 +1000

    refactor: refactor buttongroup component to map over button components (#28) (#29)
    * refactor: refactor buttongroup component to map over button components (#28)
    
    * refactor: added disabled to button (#28)
    
    * refactor: version minor (#28)
    
    ---------
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ packages/data-explorer-ui/src/components/common/ButtonGroup/components/ButtonGroupButton/buttonGroupButton.tsx
@@ -0,0 +1,26 @@
+import { Button as MButton } from "@mui/material";
+import React, { ReactNode } from "react";
+import { LoadingIcon } from "../../../CustomIcon/components/LoadingIcon/loadingIcon";
+
+export interface ButtonGroupButtonProps {
+  action: string; // Short description to describe button action.
+  disabled?: boolean;
+  label: ReactNode; // Button label may be a string or an element e.g. icon.
+  loading?: boolean;
+  loadingIcon?: ReactNode;
+  onClick: () => void; // Function invoked with button onClick handler.
+}
+
+export const ButtonGroupButton = ({
+  disabled = false,
+  label,
+  loading,
+  loadingIcon = <LoadingIcon fontSize="small" />,
+  onClick,
+}: ButtonGroupButtonProps): JSX.Element => {
+  return (
+    <MButton disabled={disabled} onClick={loading ? undefined : onClick}>
+      {loading ? loadingIcon : label}
+    </MButton>
+  );
+};
