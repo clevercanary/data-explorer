37a9e09c924f9a0bc9588c3b6b12ed9143e767e8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-03-17 13:45:27 +1000

    feat: update call to action button to take an optional button style (#45)

--- packages/data-explorer-ui/src/components/common/Button/components/CallToActionButton/callToActionButton.tsx
+++ packages/data-explorer-ui/src/components/common/Button/components/CallToActionButton/callToActionButton.tsx
@@ -1,34 +1,51 @@
 import NLink from "next/link";
-import React from "react";
+import React, { ElementType } from "react";
 import { ANCHOR_TARGET } from "../../../../Links/common/entities";
+import { isClientSideNavigation } from "../../../../Links/common/utils";
 import { ButtonPrimary } from "../../button.styles";
 
 export interface CallToAction {
   label: string;
-  target: ANCHOR_TARGET;
+  target?: ANCHOR_TARGET;
   url: string;
 }
 
 export interface CallToActionButtonProps {
+  ButtonElType?: ElementType;
   callToAction: CallToAction;
   className?: string;
+  disabled?: boolean;
 }
 
 export const CallToActionButton = ({
+  ButtonElType: Button = ButtonPrimary,
   callToAction,
   className,
+  disabled = false,
 }: CallToActionButtonProps): JSX.Element => {
   const { label, target, url } = callToAction;
-  return (
+  const isInternal = isClientSideNavigation(url);
+  return isInternal ? (
     <NLink href={url} passHref>
-      <ButtonPrimary
+      <Button
         className={className}
+        disabled={disabled}
         href="passHref"
         rel="noopener"
-        target={target}
+        target={target || ANCHOR_TARGET.SELF}
       >
         {label}
-      </ButtonPrimary>
+      </Button>
     </NLink>
+  ) : (
+    <Button
+      className={className}
+      disabled={disabled}
+      href={url}
+      rel="noopener"
+      target={target || ANCHOR_TARGET.BLANK}
+    >
+      {label}
+    </Button>
   );
 };
