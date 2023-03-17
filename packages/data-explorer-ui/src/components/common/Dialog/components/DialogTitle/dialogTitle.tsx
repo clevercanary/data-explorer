8812923cc309814e7d6bf98655477dbe4404bc21
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-03-17 17:59:04 +1000

    feat: add radio group component (#47)

--- packages/data-explorer-ui/src/components/common/Dialog/components/DialogTitle/dialogTitle.tsx
+++ packages/data-explorer-ui/src/components/common/Dialog/components/DialogTitle/dialogTitle.tsx
@@ -3,16 +3,18 @@ import { DialogTitle as MDialogTitle, IconButton } from "@mui/material";
 import React, { ReactNode } from "react";
 
 export interface DialogTitleProps {
+  className?: string;
   onClose?: () => void;
   title?: ReactNode;
 }
 
 export const DialogTitle = ({
+  className,
   onClose,
   title,
 }: DialogTitleProps): JSX.Element => {
   return (
-    <MDialogTitle>
+    <MDialogTitle className={className}>
       {title}
       {onClose && (
         <IconButton color="ink" edge="end" onClick={onClose} size="xsmall">
