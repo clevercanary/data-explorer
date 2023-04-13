246a36ecaccdcf9d44dc057091accb2dc773777b
MillenniumFalconMechanic <mim@clevercanary.com>
2023-04-13 16:46:45 -0700

    feat: track entity table page #807

--- packages/data-explorer-ui/src/common/analytics/entities.ts
+++ packages/data-explorer-ui/src/common/analytics/entities.ts
@@ -5,7 +5,7 @@ export type DataLayer = any;
  * Set of analytics event actions.
  */
 export enum EVENT_NAME {
-  ENTITY_NAME = "entity_name",
+  ENTITY_TABLE_PAGINATED = "entity_table_paginated",
   ENTITY_TABLE_SORTED = "entity_table_sorted",
 }
 
@@ -15,7 +15,16 @@ export enum EVENT_NAME {
 export enum EVENT_PARAM {
   COLUMN_NAME = "column_name",
   ENTITY_NAME = "entity_name",
+  PAGINATION_DIRECTION = "pagination_direction",
   SORT_DIRECTION = "sort_direction",
+}
+
+/**
+ * Set of analytics pagination direction values.
+ */
+export enum PAGINATION_DIRECTION {
+  NEXT = "next",
+  PREV = "prev",
 }
 
 /**
@@ -29,5 +38,14 @@ export enum SORT_DIRECTION {
 /**
  * Model of event parameters mapped to the event name.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- TODO.
-export type EventParams = any;
+export type EventParams = {
+  [EVENT_NAME.ENTITY_TABLE_PAGINATED]: {
+    [EVENT_PARAM.ENTITY_NAME]: string;
+    [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION;
+  };
+  [EVENT_NAME.ENTITY_TABLE_SORTED]: {
+    [EVENT_PARAM.ENTITY_NAME]: string;
+    [EVENT_PARAM.COLUMN_NAME]: string;
+    [EVENT_PARAM.SORT_DIRECTION]: SORT_DIRECTION;
+  };
+};
