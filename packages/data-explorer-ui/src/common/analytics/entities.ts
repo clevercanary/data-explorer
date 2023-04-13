46f6eac4df6701905a0fffb9adfe10bc4076f3f1
MillenniumFalconMechanic <mim@clevercanary.com>
2023-04-13 13:34:43 -0700

    feat: added GA4 tracking service #813

---
+++ packages/data-explorer-ui/src/common/analytics/entities.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- We can't determine the data layer type.
+export type DataLayer = any;
+
+/**
+ * Set of analytics event actions.
+ */
+export enum EVENT_NAME {}
+
+/**
+ * Set of analytics event parameters.
+ */
+export enum EVENT_PARAM {}
+
+/**
+ * Model of event parameters mapped to the event name.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- TODO.
+export type EventParams = any;
