e7c70b749fc4ee154daf4e338e7bd3eddfc6c8c8
Mim Hastie <mim@clevercanary.com>
2023-04-13 16:54:09 -0700

    feat: track entity table page #807 (#65)

--- packages/data-explorer-ui/src/components/Table/table.tsx
+++ packages/data-explorer-ui/src/components/Table/table.tsx
@@ -25,6 +25,7 @@ import { track } from "../../common/analytics/analytics";
 import {
   EVENT_NAME,
   EVENT_PARAM,
+  PAGINATION_DIRECTION,
   SORT_DIRECTION,
 } from "../../common/analytics/entities";
 import { Pagination } from "../../common/entities";
@@ -164,6 +165,11 @@ TableProps<T>): JSX.Element => {
           type: ExploreActionKind.PaginateTable,
         });
       };
+      // Execute GTM tracking.
+      track(EVENT_NAME.ENTITY_TABLE_PAGINATED, {
+        [EVENT_PARAM.ENTITY_NAME]: exploreState.tabValue,
+        [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION.NEXT,
+      });
     }
     // const nextPage = pagination?.nextPage ?? tableNextPage;
     nextPage();
@@ -180,6 +186,10 @@ TableProps<T>): JSX.Element => {
           type: ExploreActionKind.PaginateTable,
         });
       };
+      track(EVENT_NAME.ENTITY_TABLE_PAGINATED, {
+        [EVENT_PARAM.ENTITY_NAME]: exploreState.tabValue,
+        [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION.PREV,
+      });
     }
     previousPage();
     scrollTop();
