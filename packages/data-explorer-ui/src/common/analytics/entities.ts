9528f1ff5fe6364818b1f37f080442d959cbe7c7
Mim Hastie <mim@clevercanary.com>
2023-04-14 11:55:07 -0700

    feat: add GA tracking (#70)
    * feat: added entity table sort #806 (#64)
    
    * feat: added entity table sort #806
    
    * Bumped version
    
    * feat: track entity table page #807 (#65)
    
    * feat: track filter #808 (#67)
    
    * feat: add track select filter #809 (#69)

--- packages/data-explorer-ui/src/common/analytics/entities.ts
+++ packages/data-explorer-ui/src/common/analytics/entities.ts
@@ -4,15 +4,57 @@ export type DataLayer = any;
 /**
  * Set of analytics event actions.
  */
-export enum EVENT_NAME {}
+export enum EVENT_NAME {
+  ENTITY_SELECTED = "entity_selected",
+  ENTITY_TABLE_PAGINATED = "entity_table_paginated",
+  ENTITY_TABLE_SORTED = "entity_table_sorted",
+  FILTER_SELECTED = "filter_selected",
+}
 
 /**
  * Set of analytics event parameters.
  */
-export enum EVENT_PARAM {}
+export enum EVENT_PARAM {
+  COLUMN_NAME = "column_name",
+  ENTITY_NAME = "entity_name",
+  FILTER_NAME = "filter_name",
+  FILTER_VALUE = "filter_value",
+  PAGINATION_DIRECTION = "pagination_direction",
+  SORT_DIRECTION = "sort_direction",
+}
+
+/**
+ * Set of analytics pagination direction values.
+ */
+export enum PAGINATION_DIRECTION {
+  NEXT = "next",
+  PREV = "prev",
+}
+
+/**
+ * Set of analytics sort direction values.
+ */
+export enum SORT_DIRECTION {
+  ASC = "asc",
+  DESC = "desc",
+}
 
 /**
  * Model of event parameters mapped to the event name.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any -- TODO.
-export type EventParams = any;
+export type EventParams = {
+  [EVENT_NAME.ENTITY_SELECTED]: { [EVENT_PARAM.ENTITY_NAME]: string };
+  [EVENT_NAME.ENTITY_TABLE_PAGINATED]: {
+    [EVENT_PARAM.ENTITY_NAME]: string;
+    [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION;
+  };
+  [EVENT_NAME.ENTITY_TABLE_SORTED]: {
+    [EVENT_PARAM.ENTITY_NAME]: string;
+    [EVENT_PARAM.COLUMN_NAME]: string;
+    [EVENT_PARAM.SORT_DIRECTION]: SORT_DIRECTION;
+  };
+  [EVENT_NAME.FILTER_SELECTED]: {
+    [EVENT_PARAM.FILTER_NAME]: string;
+    [EVENT_PARAM.FILTER_VALUE]: string;
+  };
+};
--- packages/data-explorer-ui/src/components/Table/table.tsx
+++ packages/data-explorer-ui/src/components/Table/table.tsx
@@ -21,6 +21,13 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import React, { useEffect } from "react";
+import { track } from "../../common/analytics/analytics";
+import {
+  EVENT_NAME,
+  EVENT_PARAM,
+  PAGINATION_DIRECTION,
+  SORT_DIRECTION,
+} from "../../common/analytics/entities";
 import { Pagination } from "../../common/entities";
 import { useExploreState } from "../../hooks/useExploreState";
 import { useScroll } from "../../hooks/useScroll";
@@ -91,6 +98,14 @@ TableProps<T>): JSX.Element => {
       payload: typeof updater === "function" ? updater(sorting) : updater,
       type: ExploreActionKind.UpdateSorting,
     });
+    // Execute GTM tracking.
+    track(EVENT_NAME.ENTITY_TABLE_SORTED, {
+      [EVENT_PARAM.ENTITY_NAME]: exploreState.tabValue,
+      [EVENT_PARAM.COLUMN_NAME]: sorting[0].id,
+      [EVENT_PARAM.SORT_DIRECTION]: sorting[0].desc
+        ? SORT_DIRECTION.DESC
+        : SORT_DIRECTION.ASC,
+    });
   };
   const state = {
     pagination: {
@@ -150,6 +165,11 @@ TableProps<T>): JSX.Element => {
           type: ExploreActionKind.PaginateTable,
         });
       };
+      // Execute GTM tracking.
+      track(EVENT_NAME.ENTITY_TABLE_PAGINATED, {
+        [EVENT_PARAM.ENTITY_NAME]: exploreState.tabValue,
+        [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION.NEXT,
+      });
     }
     // const nextPage = pagination?.nextPage ?? tableNextPage;
     nextPage();
@@ -166,6 +186,10 @@ TableProps<T>): JSX.Element => {
           type: ExploreActionKind.PaginateTable,
         });
       };
+      track(EVENT_NAME.ENTITY_TABLE_PAGINATED, {
+        [EVENT_PARAM.ENTITY_NAME]: exploreState.tabValue,
+        [EVENT_PARAM.PAGINATION_DIRECTION]: PAGINATION_DIRECTION.PREV,
+      });
     }
     previousPage();
     scrollTop();
