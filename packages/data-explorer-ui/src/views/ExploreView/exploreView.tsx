efe21c6bbf9e9901e87284f82194c465b29685c4
Mim Hastie <mim@clevercanary.com>
2023-04-13 20:01:58 -0700

    feat: track filter #808 (#67)

--- packages/data-explorer-ui/src/views/ExploreView/exploreView.tsx
+++ packages/data-explorer-ui/src/views/ExploreView/exploreView.tsx
@@ -4,6 +4,8 @@ import {
   AzulEntitiesStaticResponse,
   AzulSummaryResponse,
 } from "../../apis/azul/common/entities";
+import { track } from "../../common/analytics/analytics";
+import { EVENT_NAME, EVENT_PARAM } from "../../common/analytics/entities";
 import { CategoryKey, CategoryValueKey } from "../../common/entities";
 import { Tab, Tabs, TabValue } from "../../components/common/Tabs/tabs";
 import { ComponentCreator } from "../../components/ComponentCreator/ComponentCreator";
@@ -66,6 +68,13 @@ export const ExploreView = (props: ExploreViewProps): JSX.Element => {
       },
       type: ExploreActionKind.UpdateFilter,
     });
+    // Execute GTM tracking.
+    if (selected) {
+      track(EVENT_NAME.FILTER_SELECTED, {
+        [EVENT_PARAM.FILTER_NAME]: categoryKey,
+        [EVENT_PARAM.FILTER_VALUE]: selectedCategoryValue,
+      });
+    }
   };
 
   /**
