f82509e0d4251d42152ff31f5b42f9691441835a
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-17 14:13:51 -0300

    #66 - formating file

--- packages/data-explorer-ui/src/entity/api/client.ts
+++ packages/data-explorer-ui/src/entity/api/client.ts
@@ -5,6 +5,7 @@ import { getURL } from "../../shared/utils";
  * Returns an AxiosInstance to be used to make API calls
  * @returns {AxiosInstance} with the current configs URL as baseURL
  */
-export const api = ():AxiosInstance => axios.create({
-  baseURL: getURL(),
-});
+export const api = (): AxiosInstance =>
+  axios.create({
+    baseURL: getURL(),
+  });
