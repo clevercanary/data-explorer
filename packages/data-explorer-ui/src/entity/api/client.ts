fb02ad3b5f901972cb0ad08add108b28728a3349
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-19 22:07:32 -0300

    #73 - axios timeout

--- packages/data-explorer-ui/src/entity/api/client.ts
+++ packages/data-explorer-ui/src/entity/api/client.ts
@@ -4,13 +4,14 @@ import { getURL } from "../../shared/utils";
 let axiosInstance: AxiosInstance | null = null;
 
 /**
- * Returns an AxiosInstance to be used to make API calls
+ * Returns an AxiosInstance to be used to make API calls with a timeout of 10 seconds
  * @returns {AxiosInstance} with the current configs URL as baseURL
  */
 export const api = (): AxiosInstance => {
   if (!axiosInstance) {
     axiosInstance = axios.create({
       baseURL: getURL(),
+      timeout: 10 * 1000,
     });
   }
   return axiosInstance;
