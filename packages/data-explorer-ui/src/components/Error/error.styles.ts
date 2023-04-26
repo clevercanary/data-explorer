0fa67597690c82b111a3b3bc45bdf1cde977ffa7
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-04-26 19:16:40 -0300

    feat: add error component to the package for (#68) (#94)
    feat: add error component to the package (#68)
    
    * #833 - bumping version
    
    * #68 - Moving Error component
    
    * feat: reverting version bump #68

---
+++ packages/data-explorer-ui/src/components/Error/error.styles.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { Section } from "../common/Section/section.styles";
+
+export const Error = styled.div`
+  margin: 0 auto;
+  max-width: 608px;
+  padding: 20px 0;
+  width: 100%;
+`;
+
+export const ErrorSection = styled(Section)`
+  align-items: center;
+`;
+
+export const SectionContent = styled.div`
+  text-align: center;
+`;
