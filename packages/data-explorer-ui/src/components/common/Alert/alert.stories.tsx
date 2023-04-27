263afbd4982d8bdf920ffc89bf79037383a71828
Stanislav Korolenko <mail@alaida.net>
2023-04-27 10:08:05 +0700

    feat: Add story for Alert component (#84)
    * feat: add story for Alert (#82)
    
    * feat: add detailed descriptions to alert (#82)
    
    * feat: remove deprecation note from alert variant (#82)

---
+++ packages/data-explorer-ui/src/components/common/Alert/alert.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import * as React from "react";
+import { Alert } from "./alert";
+
+export default {
+  argTypes: {
+    children: {
+      control: "text",
+      description: "Inner text",
+    },
+    color: {
+      description: "Color of alert",
+    },
+    icon: {
+      control: "text",
+      description: "Top left icon. If provided will replace severity.",
+    },
+    severity: {
+      description: "Top left predefined icon. Combine with color.",
+    },
+    title: {
+      control: "text",
+      description: "Highlighted text",
+    },
+    variant: {
+      description: "Type of the alert",
+    },
+  },
+  component: Alert,
+  title: "Components/Common/Alert",
+} satisfies ComponentMeta<typeof Alert>;
+
+const AlertTemplate: ComponentStory<typeof Alert> = (args) => (
+  <Alert {...args} />
+);
+
+export const AlertStory = AlertTemplate.bind({});
+AlertStory.args = {
+  children: "Something certainly happened",
+  color: "warning",
+  severity: "warning",
+  title: "Ouch!",
+};
