8d1e54790c7e6742f3b07d4548b899625f08d61b
Fran McDade <frano-m@users.noreply.github.com>
2023-05-03 16:31:57 +1000

    feat: update citation component prop projectpath to projecturl (#141) (#146)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- packages/data-explorer-ui/src/components/Project/components/Citation/citation.stories.tsx
+++ packages/data-explorer-ui/src/components/Project/components/Citation/citation.stories.tsx
@@ -1,20 +1,21 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
 import { Citation } from "./citation";
 
-export default {
+const meta: Meta<typeof Citation> = {
   argTypes: {
     projectPath: { control: "text" },
+    url: { control: "text" },
   },
   component: Citation,
   title: "Components/SectionContent/Content/Project",
-} as ComponentMeta<typeof Citation>;
+};
 
-const CitationTemplate: ComponentStory<typeof Citation> = (args) => (
-  <Citation {...args} />
-);
+export default meta;
+type Story = StoryObj<typeof Citation>;
 
-export const CitationStory = CitationTemplate.bind({});
-CitationStory.args = {
-  projectPath: "/60ea42e1-af49-42f5-8164-d641fdb696bc",
+export const CitationStory: Story = {
+  args: {
+    projectPath: "60ea42e1-af49-42f5-8164-d641fdb696bc",
+    url: "https://data.humancellatlas.org/explore/projects/",
+  },
 };
