a92eda4ed91ea372d9de4c0749108701a4aa0850
Ajalle <mail@alaida.net>
2023-05-08 16:31:30 +0800

    feat: add copy to clipboard functionality to code component (#168)

--- packages/data-explorer-ui/src/components/common/Code/code.styles.ts
+++ packages/data-explorer-ui/src/components/common/Code/code.styles.ts
@@ -1,7 +1,36 @@
 import styled from "@emotion/styled";
+import { textBody500 } from "../../../styles/common/mixins/fonts";
+import { fontFamily } from "../../../theme/common/typography";
 
-export const CodeBlock = styled("pre")`
+interface CodeBlockProps {
+  copied?: boolean;
+}
+
+export const CodeBlock = styled("pre")<CodeBlockProps>`
   background: ${({ theme }) => theme.palette.info.lightest};
   border-radius: ${({ theme }) => theme.spacing(1)};
   padding: ${({ theme }) => theme.spacing(4)};
+  position: relative;
+  user-select: none;
+
+  &:hover::after {
+    opacity: 1;
+  }
+
+  &::after {
+    ${textBody500};
+    font-family: ${fontFamily};
+    align-items: center;
+    background: ${({ copied }) =>
+      copied ? "rgba(28, 124, 199)" : "rgba(28, 124, 199, 0.9)"};
+    border-radius: ${({ theme }) => theme.spacing(1)};
+    color: ${({ theme }) => theme.palette.common.white};
+    content: ${({ copied }) => (copied ? "'Copied'" : "'Copy to clipboard'")};
+    display: flex;
+    inset: 0;
+    justify-content: center;
+    position: absolute;
+    opacity: 0;
+    transition: opacity ${({ theme }) => theme.transitions.duration.standard}ms;
+  }
 `;
--- packages/data-explorer-ui/src/components/common/Code/code.tsx
+++ packages/data-explorer-ui/src/components/common/Code/code.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import copy from "copy-to-clipboard";
+import React, { useEffect, useRef, useState } from "react";
 import { CodeBlock } from "./code.styles";
 
 /**
@@ -10,8 +11,28 @@ export interface CodeProps {
 }
 
 export const Code = ({ code }: CodeProps): JSX.Element => {
+  const [copied, setCopied] = useState(false);
+  const timeoutRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    // cleanup function
+    return () => {
+      if (timeoutRef.current !== null) {
+        window.clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  const onClick = (): void => {
+    copy(code);
+    setCopied(true);
+    timeoutRef.current = window.setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
   return (
-    <CodeBlock>
+    <CodeBlock onClick={onClick} copied={copied}>
       <code>{code}</code>
     </CodeBlock>
   );
