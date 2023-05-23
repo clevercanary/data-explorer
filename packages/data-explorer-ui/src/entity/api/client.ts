f49c2466289bb699d192af84a7448e0e4106153b
Bruce Rodrigues <brucerodrigues6@gmail.com>
2023-05-22 22:47:24 -0300

    feat: fixing interceptor creator to after config is set #857

--- packages/data-explorer-ui/src/entity/api/client.ts
+++ packages/data-explorer-ui/src/entity/api/client.ts
@@ -9,6 +9,29 @@ import { getURL } from "../../shared/utils";
 let axiosInstance: AxiosInstance | null = null;
 
 /**
+ * Adding response interceptors to axios instances
+ * @param api - instance of axios that will receive response interceptors
+ */
+const configureInterceptors = (api: AxiosInstance): void => {
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error: AxiosError) => {
+      const { config, response } = error;
+
+      if (response?.status === HttpStatusCode.ServiceUnavailable && config) {
+        const retryAfterValue = response.headers["Retry-After"];
+        const waitingTime = retryAfterValue ? +retryAfterValue : 0;
+        return new Promise((resolve) => {
+          setTimeout(() => resolve(api(config)), waitingTime);
+        });
+      }
+
+      Promise.reject(error);
+    }
+  );
+};
+
+/**
  * Returns an AxiosInstance to be used to make API calls with a timeout of 10 seconds
  * @returns {AxiosInstance} with the current configs URL as baseURL
  */
@@ -18,26 +41,7 @@ export const api = (): AxiosInstance => {
       baseURL: getURL(),
       timeout: 10 * 1000,
     });
+    configureInterceptors(axiosInstance);
   }
   return axiosInstance;
 };
-
-/**
- * Adding interceptors to axios responses
- */
-api().interceptors.response.use(
-  (response: AxiosResponse) => response,
-  (error: AxiosError) => {
-    const { config, response } = error;
-
-    if (response?.status === HttpStatusCode.ServiceUnavailable && config) {
-      const retryAfterValue = response.headers["Retry-After"];
-      const waitingTime = retryAfterValue ? +retryAfterValue : 0;
-      return new Promise((resolve) => {
-        setTimeout(() => resolve(api()(config)), waitingTime);
-      });
-    }
-
-    Promise.reject(error);
-  }
-);
