45d6dbb4b3745b7e20a9f21d704d048ccf7d28b5
Fran McDade <frano-m@users.noreply.github.com>
2023-06-20 14:42:51 +1000

    feat: update detailviewtable and noresults with paper prop (#252) (#253)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- packages/data-explorer-ui/src/components/Detail/components/DetailViewTable/detailViewTable.tsx
+++ packages/data-explorer-ui/src/components/Detail/components/DetailViewTable/detailViewTable.tsx
@@ -1,27 +1,36 @@
 import { ColumnDef } from "@tanstack/react-table";
 import React, { ReactNode } from "react";
-import { GridPaper, RoundedPaper } from "../../../common/Paper/paper.styles";
+import {
+  FlatPaper,
+  FluidPaper,
+  GridPaper,
+  RoundedPaper,
+} from "../../../common/Paper/paper.styles";
 import { NoResults } from "../../../NoResults/noResults";
 import { TableToolbar } from "../../../Table/table.styles";
 import { Table } from "../Table/table";
 
 interface DetailViewTableProps<T extends object> {
+  className?: string;
   columns: ColumnDef<T>[];
   gridTemplateColumns: string;
   items: T[];
   noResultsTitle: string;
+  Paper?: typeof FlatPaper | typeof FluidPaper | typeof RoundedPaper;
   tools?: ReactNode;
 }
 
 export const DetailViewTable = <T extends object>({
+  className,
   columns,
   gridTemplateColumns,
   items,
   noResultsTitle,
+  Paper = RoundedPaper,
   tools,
 }: DetailViewTableProps<T>): JSX.Element => {
   return items.length > 0 ? (
-    <RoundedPaper>
+    <Paper className={className}>
       <GridPaper>
         {tools && <TableToolbar>{tools}</TableToolbar>}
         <Table
@@ -30,8 +39,8 @@ export const DetailViewTable = <T extends object>({
           items={items}
         />
       </GridPaper>
-    </RoundedPaper>
+    </Paper>
   ) : (
-    <NoResults title={noResultsTitle} />
+    <NoResults Paper={Paper} title={noResultsTitle} />
   );
 };
--- packages/data-explorer-ui/src/components/NoResults/noResults.tsx
+++ packages/data-explorer-ui/src/components/NoResults/noResults.tsx
@@ -1,7 +1,11 @@
 import { Typography } from "@mui/material";
 import React, { ReactNode } from "react";
 import { SearchOffIcon } from "../common/CustomIcon/components/SearchOffIcon/searchOffIcon";
-import { RoundedPaper } from "../common/Paper/paper.styles";
+import {
+  FlatPaper,
+  FluidPaper,
+  RoundedPaper,
+} from "../common/Paper/paper.styles";
 import { SectionTitle } from "../common/Section/components/SectionTitle/sectionTitle";
 import { SectionActions } from "../common/Section/section.styles";
 import { PRIORITY, StatusIcon } from "../common/StatusIcon/statusIcon";
@@ -10,16 +14,18 @@ import { NoResultsSection, NoResultsSectionContent } from "./noResults.styles";
 export interface NoResultsProps {
   actions?: ReactNode;
   description?: string;
+  Paper?: typeof FlatPaper | typeof FluidPaper | typeof RoundedPaper;
   title: string;
 }
 
 export const NoResults = ({
   actions,
   description,
+  Paper = RoundedPaper,
   title,
 }: NoResultsProps): JSX.Element => {
   return (
-    <RoundedPaper>
+    <Paper>
       <NoResultsSection>
         <StatusIcon priority={PRIORITY.LOW} StatusIcon={SearchOffIcon} />
         <NoResultsSectionContent>
@@ -32,6 +38,6 @@ export const NoResults = ({
         </NoResultsSectionContent>
         {actions && <SectionActions>{actions}</SectionActions>}
       </NoResultsSection>
-    </RoundedPaper>
+    </Paper>
   );
 };
