810444f2fbdacb03c2aceaa0b4d9a3b2cdb588c3
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-10-27 16:38:38 +1000

    feat: provided unique identifier for the collapsable table rows (#427)

--- packages/data-explorer-ui/src/components/Detail/components/Table/components/TableRows/components/CollapsableRows/collapsableRows.tsx
+++ packages/data-explorer-ui/src/components/Detail/components/Table/components/TableRows/components/CollapsableRows/collapsableRows.tsx
@@ -1,6 +1,7 @@
 import { TableRow } from "@mui/material";
 import { Table } from "@tanstack/react-table";
 import React, { Fragment } from "react";
+import { v4 as uuid4 } from "uuid";
 import { CollapsableCell } from "../../../../../../../Table/components/TableCell/components/CollapsableCell/collapsableCell";
 
 export interface CollapsableRowsProps<T> {
@@ -12,11 +13,12 @@ export const CollapsableRows = <T extends object>({
 }: CollapsableRowsProps<T>): JSX.Element => {
   const { getRowModel } = tableInstance;
   const { rows } = getRowModel();
+  const uuid = uuid4(); // Generate a unique ID for the collapsable rows.
   return (
     <Fragment>
       {rows.map((row) => {
         return (
-          <TableRow key={row.id}>
+          <TableRow key={`${uuid}${row.id}`}>
             <CollapsableCell row={row} />
           </TableRow>
         );
