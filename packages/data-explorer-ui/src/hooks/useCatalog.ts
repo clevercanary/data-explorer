3717c8871b30d83d1bb329d17696844cb2b87103
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-07 16:33:43 +1000

    feat: only specify catalog # once (not twice) (#290)

--- packages/data-explorer-ui/src/hooks/useCatalog.ts
+++ packages/data-explorer-ui/src/hooks/useCatalog.ts
@@ -1,15 +1,13 @@
 import { useConfig } from "./useConfig";
 
 /**
- * Returns configured entity list and detail catalog values.
- * @returns entity list and detail catalog values.
+ * Returns configured catalog value.
+ * @returns catalog value.
  */
-export const useCatalog = (): [string, string] => {
+export const useCatalog = (): string | undefined => {
   const { config } = useConfig();
   const { dataSource } = config;
-  const { defaultDetailParams, defaultListParams } = dataSource || {};
-  const { catalog: detailCatalog } = defaultDetailParams || {};
-  const { catalog: listCatalog } = defaultListParams || {};
-  // Returns entity list and detail catalog configuration.
-  return [listCatalog, detailCatalog];
+  const { defaultParams } = dataSource || {};
+  const { catalog } = defaultParams || {};
+  return catalog;
 };
