fcd031a55f86165c3c3ee50a4eb81b1fc2873fdd
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-11-08 15:13:55 -0800

    feat: adjust filter tag tooltip (#438)
    * calculate overflow only after initial render
    * disableInteractive
    * adjust position downward

--- packages/data-explorer-ui/src/components/Filter/components/FilterTag/filterTag.tsx
+++ packages/data-explorer-ui/src/components/Filter/components/FilterTag/filterTag.tsx
@@ -1,7 +1,6 @@
 import { CloseRounded } from "@mui/icons-material";
 import { Chip, Tooltip } from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
-import { useWindowResize } from "../../../../hooks/useWindowResize";
 import { SupersededTag } from "./filterTag.styles";
 
 export interface FilterTagProps {
@@ -10,6 +9,19 @@ export interface FilterTagProps {
   superseded: boolean;
 }
 
+const DEFAULT_SLOT_PROPS = {
+  popper: {
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [0, -6],
+        },
+      },
+    ],
+  },
+};
+
 export const FilterTag = ({
   label,
   onRemove,
@@ -17,16 +29,21 @@ export const FilterTag = ({
 }: FilterTagProps): JSX.Element => {
   const Tag = superseded ? SupersededTag : Chip;
   const tagRef = useRef<HTMLDivElement>(null);
-  const windowSize = useWindowResize();
   const [isOverflowed, setIsOverflowed] = useState(false);
   useEffect(() => {
     const tagLabelElement =
       tagRef.current?.querySelector<HTMLElement>(".MuiChip-label");
     if (!tagLabelElement) return;
     setIsOverflowed(tagLabelElement.offsetWidth < tagLabelElement.scrollWidth);
-  }, [windowSize]);
+  }, []);
   return (
-    <Tooltip arrow placement="top" title={isOverflowed ? label : null}>
+    <Tooltip
+      arrow
+      disableInteractive
+      placement="top"
+      slotProps={DEFAULT_SLOT_PROPS}
+      title={isOverflowed ? label : null}
+    >
       <Tag
         clickable={false} // removes unwanted active and hover ui; "pointer" cursor added to "filterTag" variant in theme.
         color="primary"
