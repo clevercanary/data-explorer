2d1508635e34bdce2671b2c93fa78a76992702a0
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2023-11-08 16:24:57 -0800

    feat: add tooltip when filter tag label is truncated (#438) (#458)
    * feat: add tooltip when filter tag label is truncated (#438)
    
    * refactor: import hooks by name (#438)
    
    * feat: adjust filter tag tooltip (#438)
    
    * calculate overflow only after initial render
    * disableInteractive
    * adjust position downward
    
    * refactor: remove unnecessary useEffect (#438)

--- packages/data-explorer-ui/src/components/Filter/components/FilterTag/filterTag.tsx
+++ packages/data-explorer-ui/src/components/Filter/components/FilterTag/filterTag.tsx
@@ -1,6 +1,6 @@
 import { CloseRounded } from "@mui/icons-material";
-import { Chip } from "@mui/material";
-import React from "react";
+import { Chip, Tooltip } from "@mui/material";
+import React, { useRef } from "react";
 import { SupersededTag } from "./filterTag.styles";
 
 export interface FilterTagProps {
@@ -9,21 +9,48 @@ export interface FilterTagProps {
   superseded: boolean;
 }
 
+const DEFAULT_SLOT_PROPS = {
+  popper: {
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [0, -6],
+        },
+      },
+    ],
+  },
+};
+
 export const FilterTag = ({
   label,
   onRemove,
   superseded,
 }: FilterTagProps): JSX.Element => {
   const Tag = superseded ? SupersededTag : Chip;
+  const tagRef = useRef<HTMLDivElement>(null);
+  const tagLabelElement =
+    tagRef.current?.querySelector<HTMLElement>(".MuiChip-label");
+  const isOverflowed =
+    (tagLabelElement?.offsetWidth ?? 0) < (tagLabelElement?.scrollWidth ?? 0);
   return (
-    <Tag
-      clickable={false} // removes unwanted active and hover ui; "pointer" cursor added to "filterTag" variant in theme.
-      color="primary"
-      deleteIcon={<CloseRounded color="inherit" />}
-      label={label}
-      onClick={onRemove}
-      onDelete={onRemove}
-      variant="filterTag"
-    />
+    <Tooltip
+      arrow
+      disableInteractive
+      placement="top"
+      slotProps={DEFAULT_SLOT_PROPS}
+      title={isOverflowed ? label : null}
+    >
+      <Tag
+        clickable={false} // removes unwanted active and hover ui; "pointer" cursor added to "filterTag" variant in theme.
+        color="primary"
+        deleteIcon={<CloseRounded color="inherit" />}
+        label={label}
+        onClick={onRemove}
+        onDelete={onRemove}
+        ref={tagRef}
+        variant="filterTag"
+      />
+    </Tooltip>
   );
 };
