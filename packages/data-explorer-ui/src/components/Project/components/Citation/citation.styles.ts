fec9925b4cba86d62307d5f4964a5d7735e34b4c
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-10 19:56:47 +1000

    fix: fix citation on mobile (#464)

--- packages/data-explorer-ui/src/components/Project/components/Citation/citation.styles.ts
+++ packages/data-explorer-ui/src/components/Project/components/Citation/citation.styles.ts
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 
+export const SectionContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  min-width: 0; /* required to ellipsis any flex child */
+  width: 100%; /* required to ellipsis any flex child */
+`;
+
 export const CitationLink = styled.div`
-  white-space: nowrap;
+  align-items: center;
+  display: flex;
+  min-width: 0;
 
   // Copy icon button
   & button {
