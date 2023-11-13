32749f185f3457e6ffa0de7b0d7dadb1dd6f5ab7
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-11-13 16:14:23 +1000

    fix: update entity detail view to only fetch entity when required (#469)

--- packages/data-explorer-ui/src/hooks/useFetchEntity.tsx
+++ packages/data-explorer-ui/src/hooks/useFetchEntity.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { RESPONSE_SOURCE } from "../apis/azul/common/entities";
 import { PARAMS_INDEX_UUID } from "../common/constants";
 import { EntityDetailViewProps } from "../views/EntityDetailView/entityDetailView";
 import { useAsync } from "./useAsync";
 import { useAuthentication } from "./useAuthentication";
+import { useConfig } from "./useConfig";
 import { useEntityService } from "./useEntityService";
 
 interface UseEntityDetailResponse<T> {
@@ -21,23 +22,34 @@ interface UseEntityDetailResponse<T> {
 export const useFetchEntity = <T,>(
   detailViewProps?: EntityDetailViewProps
 ): UseEntityDetailResponse<T> => {
-  const { data: entityList } = detailViewProps || {}; // Data is statically loaded if entity list is defined.
+  const { data: entityList, entityListType } = detailViewProps || {}; // Data is statically loaded if entity list is defined.
+  const { authentication } = useConfig().config;
   const { token } = useAuthentication();
-  const { fetchEntityDetail, listStaticLoad, path } = useEntityService();
+  const { fetchEntityDetail, listStaticLoad, path } =
+    useEntityService(entityListType);
   const {
     query: { params },
   } = useRouter();
   const uuid = params?.[PARAMS_INDEX_UUID] as string;
   const { data: response, isIdle, isLoading, run } = useAsync<T>();
   const isIdleOrLoading = isIdle || isLoading;
-  const shouldFetchEntityDetail = !listStaticLoad && !response;
+  const shouldFetchEntity = useMemo(
+    () =>
+      isFetchRequired(
+        listStaticLoad,
+        Boolean(authentication && token),
+        Boolean(entityList),
+        Boolean(response)
+      ),
+    [authentication, entityList, listStaticLoad, response, token]
+  );
 
   useEffect(() => {
     // Fetch entity if entity data originates from a request, and has not yet been requested.
-    if (shouldFetchEntityDetail && uuid) {
+    if (shouldFetchEntity && uuid) {
       run(fetchEntityDetail(uuid, path, token));
     }
-  }, [fetchEntityDetail, path, run, shouldFetchEntityDetail, token, uuid]);
+  }, [fetchEntityDetail, path, run, shouldFetchEntity, token, uuid]);
 
   if (token) {
     return {
@@ -56,3 +68,29 @@ export const useFetchEntity = <T,>(
     response: entityList ? entityList : response,
   };
 };
+
+/**
+ * Returns true if a fetch is necessary.
+ * @param listStaticLoad - Flag indicating if the entity list is statically loaded.
+ * @param isAuthenticated - Flag indicating if authentication is enabled.
+ * @param hasStaticResponse - Flag indicating if a statically loaded response exists.
+ * @param hasFetchResponse - Flag indicating if a response exists.
+ * @returns true if a fetch is necessary.
+ */
+function isFetchRequired(
+  listStaticLoad: boolean,
+  isAuthenticated: boolean,
+  hasStaticResponse: boolean,
+  hasFetchResponse: boolean
+): boolean {
+  if (listStaticLoad) {
+    return false;
+  }
+  if (hasStaticResponse) {
+    if (isAuthenticated) {
+      return !hasFetchResponse;
+    }
+    return false;
+  }
+  return !hasFetchResponse;
+}
