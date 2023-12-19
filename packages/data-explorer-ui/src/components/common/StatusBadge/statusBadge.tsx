58c7ac5d0fca75a3e583e53427350a9ae5a985e1
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2023-12-19 14:35:34 +1000

    feat: create new accessibility status for entity detail (#541)

--- packages/data-explorer-ui/src/components/common/StatusBadge/statusBadge.tsx
+++ packages/data-explorer-ui/src/components/common/StatusBadge/statusBadge.tsx
@@ -1,5 +1,5 @@
 import { Chip as MChip, ChipProps as MChipProps } from "@mui/material";
-import React from "react";
+import React, { forwardRef } from "react";
 
 export enum STATUS_BADGE_COLOR {
   DEFAULT = "default",
@@ -9,12 +9,27 @@ export enum STATUS_BADGE_COLOR {
   WARNING = "warning",
 }
 
-export const StatusBadge = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- children is null for Mui ChipProps.
-  children,
-  color,
-  label,
-  ...props /* Spread props to allow for Mui ChipProps specific prop overrides e.g. "onDelete". */
-}: MChipProps): JSX.Element => {
-  return <MChip color={color} label={label} variant="status" {...props} />;
-};
+export const StatusBadge = forwardRef<HTMLDivElement, MChipProps>(
+  function StatusBadge(
+    {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars -- children is null for Mui ChipProps.
+      children,
+      className,
+      color,
+      label,
+      ...props /* Spread props to allow for Mui ChipProps specific prop overrides e.g. "onDelete". */
+    }: MChipProps,
+    ref
+  ): JSX.Element {
+    return (
+      <MChip
+        className={className}
+        color={color}
+        label={label}
+        ref={ref}
+        variant="status"
+        {...props}
+      />
+    );
+  }
+);
