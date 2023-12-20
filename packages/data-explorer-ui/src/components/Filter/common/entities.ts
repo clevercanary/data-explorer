caba7243540dac4903e52718071f09d9c4616811
hunterckx <118154470+hunterckx@users.noreply.github.com>
2023-12-20 00:01:39 -0800

    feat: basic generalized filter search matching (#407)

--- packages/data-explorer-ui/src/components/Filter/common/entities.ts
+++ packages/data-explorer-ui/src/components/Filter/common/entities.ts
@@ -1,6 +1,28 @@
 import { Dispatch, SetStateAction } from "react";
+import { SelectCategoryValueView } from "../../../common/entities";
 
 /**
  * Function invoked to update state for the search term.
  */
 export type SetSearchTermFn = Dispatch<SetStateAction<string>>;
+
+export type FilterMenuSearchMatchRange = [number, number];
+
+export interface FilterMenuSearchStringMatch {
+  ranges: FilterMenuSearchMatchRange[];
+  score: number;
+}
+
+export type FilterMenuSearchMatchStringFn = (
+  s: string
+) => FilterMenuSearchStringMatch | false;
+
+export interface FilterMenuSearchMatch {
+  ranges?: FilterMenuSearchMatchRange[];
+  score: number;
+  value: SelectCategoryValueView;
+}
+
+export type FilterMenuSearchSortMatchesFn = (
+  values: SelectCategoryValueView[]
+) => FilterMenuSearchMatch[];
--- packages/data-explorer-ui/src/components/Filter/components/FilterMenu/filterMenu.tsx
+++ packages/data-explorer-ui/src/components/Filter/components/FilterMenu/filterMenu.tsx
@@ -6,6 +6,7 @@ import {
 import { OnFilterFn } from "../../../../hooks/useCategoryFilter";
 import { SouthIcon } from "../../../common/CustomIcon/components/SouthIcon/southIcon";
 import { MAX_DISPLAYABLE_LIST_ITEMS } from "../../common/constants";
+import { getSortMatchesFn } from "../../common/utils";
 import { List } from "../FilterList/filterList.styles";
 import { FilterMenuSearch } from "../FilterMenuSearch/filterMenuSearch";
 import { FilterNoResultsFound } from "../FilterNoResultsFound/filterNoResultsFound";
@@ -78,11 +79,7 @@ export function applyMenuFilter(
   values: SelectCategoryValueView[],
   searchTerm: string
 ): SelectCategoryValueView[] {
-  if (!searchTerm) return values;
-  searchTerm = searchTerm.toLowerCase();
-  return values.filter(
-    ({ key, label }) =>
-      key?.toLowerCase().includes(searchTerm) ||
-      label?.toLowerCase().includes(searchTerm)
-  );
+  const sortMatches = getSortMatchesFn(searchTerm);
+  if (!sortMatches) return values;
+  return sortMatches(values).map(({ value }) => value);
 }
