d3f6b7d77997c22f27488557bf415d50a8ad484b
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-02-21 15:10:34 +1000

    feat: updated axios with response inceptors (#857)

--- packages/data-explorer-ui/src/entity/common/client.ts
+++ packages/data-explorer-ui/src/entity/common/client.ts
@@ -1,7 +1,35 @@
-import axios, { AxiosInstance } from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  HttpStatusCode,
+} from "axios";
 import { getURL } from "../../shared/utils";
 
 let axiosInstance: AxiosInstance | null = null;
+
+/**
+ * Adding response interceptors to axios instances.
+ * @param api - AxiosInstance.
+ */
+export const configureInterceptors = (api: AxiosInstance): void => {
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error: AxiosError) => {
+      const { config, response } = error;
+
+      if (response?.status === HttpStatusCode.ServiceUnavailable && config) {
+        const retryAfterValue = response.headers["Retry-After"];
+        const waitingTime = retryAfterValue ? +retryAfterValue : 0;
+        return new Promise((resolve) => {
+          setTimeout(() => resolve(api(config)), waitingTime);
+        });
+      }
+
+      Promise.reject(error);
+    }
+  );
+};
 
 /**
  * Returns a singleton Axios instance configured for making HTTP requests to a specified base URL.
@@ -14,6 +42,7 @@ export const api = (baseURL = getURL()): AxiosInstance => {
       baseURL,
       timeout: 20 * 1000,
     });
+    configureInterceptors(axiosInstance);
   }
   return axiosInstance;
 };
